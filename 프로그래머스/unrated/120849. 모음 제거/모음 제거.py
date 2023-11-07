fec4b2861f2b93741b72e6e37f97834d74cf8078
jaeyoungmun <113167437+jaeyoungmun@users.noreply.github.com>
2023-11-07 17:33:26 +0900

    [level unrated] Title: 모음 제거, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120849. 모음 제거/모음 제거.py
+++ 프로그래머스/unrated/120849. 모음 제거/모음 제거.py
@@ -2,6 +2,6 @@ def solution(my_string):
     for i in my_string:
         if i in 'aeiou':
             num = my_string.index(i)
-            print(i)
+            # print(i)
             my_string = my_string[:num] + my_string[num+1:]
     return my_string
