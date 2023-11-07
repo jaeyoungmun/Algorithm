50e7823b7c3d690fa4e1372d31d5c5c73641cf8f
jaeyoungmun <113167437+jaeyoungmun@users.noreply.github.com>
2023-11-07 17:33:11 +0900

    [level unrated] Title: 모음 제거, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120849. 모음 제거/모음 제거.py
@@ -0,0 +1,7 @@
+def solution(my_string):
+    for i in my_string:
+        if i in 'aeiou':
+            num = my_string.index(i)
+            print(i)
+            my_string = my_string[:num] + my_string[num+1:]
+    return my_string
