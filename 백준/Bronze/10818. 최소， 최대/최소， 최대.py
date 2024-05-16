9736e4ab0d985392a16d376f456aac7ef7dfc4b6
jaeyoungmun <113167437+jaeyoungmun@users.noreply.github.com>
2024-05-16 23:42:09 +0900

    [Bronze III] Title: 최소, 최대, Time: 448 ms, Memory: 152796 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.py
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.py
@@ -2,13 +2,5 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-a = (input().split())
-max = int(a[0])
-min = int(a[0])
-for i in range(n):
-    a[i] = int(a[i])
-    if max < a[i]:
-        max = a[i]
-    elif min > a[i]:
-        min = a[i]
-print(f"{min} {max}")
+A = list(map(int,input().split()))
+print(f'{min(A)} {max(A)}')
