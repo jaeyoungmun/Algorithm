5b9edaaccb4e816434965b7fd066e8abd7e2cecb
jaeyoungmun <113167437+jaeyoungmun@users.noreply.github.com>
2024-10-13 22:17:24 +0900

    [Bronze III] Title: 별 찍기 - 15, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10990. 별 찍기 － 15/별 찍기 － 15.py
+++ 백준/Bronze/10990. 별 찍기 － 15/별 찍기 － 15.py
@@ -1,5 +1,14 @@
 n = int(input())
-
+# for i in range(n):
+#     for j in range(n-i-1,0,-1):
+#         print(' ',end=' ')
+#     print('*',end=' ')
+#     for k in range(2*i-1):
+#         print(' ',end=' ')
+#     if (i != 1):
+#         print('*',end=' ')
+#     print( )
+#     왜 이거는 출력 형식이 잘못되었습니다 라고 하는가...
 for i in range(n):
     print(' ' * (n-i-1) + '*',end='')
     if i>0:
